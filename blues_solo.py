ecb2fb8d4fe8b8029a5b240ce798ad76b5691ecb
Oliver Steele <steele@osteele.com>
2017-12-19 08:47:01 -0500

    Wrap long lines; make constant names uppercase

--- blues_solo.py
+++ blues_solo.py
@@ -17,11 +17,12 @@ def play_note(note, beats=1, bpm=60, amp=1):
     """Play note for `beats` beats. Return when done."""
     # `note` is this many half-steps higher than the sampled note
     half_steps = note - SAMPLE_NOTE
-    # An octave higher is twice the frequency. There are twelve half-steps per octave. Ergo,
-    # each half step is a twelth root of 2 (in equal temperament).
+    # An octave higher is twice the frequency. There are twelve half-steps per
+    # octave. Ergo, each half step is a twelth root of 2 (in equal temperament).
     rate = (2 ** (1 / 12)) ** half_steps
     assert os.path.exists(SAMPLE_FILE)
-    # Turn sample into an absolute path, since Sonic Pi is executing from a different working directory.
+    # Turn sample into an absolute path, since Sonic Pi is executing from a
+    # different working directory.
     sample(os.path.realpath(SAMPLE_FILE), rate=rate, amp=amp)
     sleep(beats * 60 / bpm)
 
@@ -37,9 +38,10 @@ def stop():
 # stop all tracks when the program exits normally or is interrupted
 atexit.register(stop)
 
-# These are the piano key numbers for a 3-octave blues scale in A. See: http://en.wikipedia.org/wiki/Blues_scale
-blues_scale = [
+# These are the piano key numbers for a 3-octave blues scale in A.
+# See: http://en.wikipedia.org/wiki/Blues_scale
+BLUES_SCALE = [
     40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
-beats_per_minute = 45				# Let's make a slow blues solo
+BEATS_PER_MINUTE = 45				# Let's make a slow blues solo
 
-play_note(blues_scale[0], beats=1, bpm=beats_per_minute)
+play_note(BLUES_SCALE[0], beats=1, bpm=BEATS_PER_MINUTE)
